780ea9fdda107ec4b9e1a7ae2332e33e83b7a8ae
RafaelRibeirodeCampos <161260581+RafaelRibeirodeCampos@users.noreply.github.com>
2024-03-25 10:45:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const frutas = ["Maçã", "Banana", "Melão"];
+frutas.push("Morango")
+//console.log(frutas[2]);
+for(let cont = 0;cont < frutas.length;cont++){
+    console.log('(for)', frutas[cont]);
+}
+function processa(item){
+    console.log('(function)', item);
+}
+const retornoForEach = frutas.forEach(function(f) {
+    console.log('(forEach)',f);
+    return f.toUpperCase();
+});
+const retornoMap = frutas.map((f) => {
+    console.log('(map)', f);
+    return f.toUpperCase();
+});
+frutas.forEach((f) => processa(f));
+console.log(retornoForEach);
+console.log(retornoMap);
